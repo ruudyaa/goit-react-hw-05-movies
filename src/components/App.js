86623b946c826badd1e86f2101a755aa053d5ce5
ruudyaa <martarudnytska26@gmail.com>
2023-06-18 20:17:40 +0300

    Adds content for App.js

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./Layout";
+
+const Home = lazy(() => import("../pages/Home"));
+const Movies = lazy(() => import("../pages/Movies"));
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+const Cast = lazy(() => import("../components/Cast/Cast"));
+const Reviews = lazy(() => import("../components/Reviews/Review"));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
